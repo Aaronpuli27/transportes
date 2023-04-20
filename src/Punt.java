3cbab7cd71c041d75b7c041f1c5691e23cf0132c
Ericfl4 <fernandezlunaeric@elpuig.xeill.net>
2023-04-20 17:06:39 +0200

    Punt hecho

---
+++ src/Punt.java
@@ -0,0 +1,87 @@
+public class Punt {
+    String ED50_COORD_X;
+    String ED50_COORD_Y;
+    String ETRS89_COORD_X;
+    String ETRS89_COORD_Y;
+    String Longitud;
+    String Latitud;
+    String icono;
+    String Tooltip;
+    String URL;
+
+    public Punt() {
+    }
+
+    public String getED50_COORD_X() {
+        return ED50_COORD_X;
+    }
+
+    public void setED50_COORD_X(String ED50_COORD_X) {
+        this.ED50_COORD_X = ED50_COORD_X;
+    }
+
+    public String getED50_COORD_Y() {
+        return ED50_COORD_Y;
+    }
+
+    public void setED50_COORD_Y(String ED50_COORD_Y) {
+        this.ED50_COORD_Y = ED50_COORD_Y;
+    }
+
+    public String getETRS89_COORD_X() {
+        return ETRS89_COORD_X;
+    }
+
+    public void setETRS89_COORD_X(String ETRS89_COORD_X) {
+        this.ETRS89_COORD_X = ETRS89_COORD_X;
+    }
+
+    public String getETRS89_COORD_Y() {
+        return ETRS89_COORD_Y;
+    }
+
+    public void setETRS89_COORD_Y(String ETRS89_COORD_Y) {
+        this.ETRS89_COORD_Y = ETRS89_COORD_Y;
+    }
+
+    public String getLongitud() {
+        return Longitud;
+    }
+
+    public void setLongitud(String longitud) {
+        Longitud = longitud;
+    }
+
+    public String getLatitud() {
+        return Latitud;
+    }
+
+    public void setLatitud(String latitud) {
+        Latitud = latitud;
+    }
+
+    public String getIcono() {
+        return icono;
+    }
+
+    public void setIcono(String icono) {
+        this.icono = icono;
+    }
+
+    public String getTooltip() {
+        return Tooltip;
+    }
+
+    public void setTooltip(String tooltip) {
+        Tooltip = tooltip;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+}
+
